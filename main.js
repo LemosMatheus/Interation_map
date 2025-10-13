1c7d664d97099b6c57781cf7cd005ce8bb011c56
0-Rafael <andrerafaelduval@gmail.com>
2025-10-13 20:25:13 -0300

    Alteração da animação do personagem

--- main.js
+++ main.js
@@ -4,20 +4,14 @@ const link3 = document.getElementById("link3")
 const link4 = document.getElementById("link4")
 const perso = document.getElementById("personagem")
 let fases = 0
-
 // link1.addEventListener("click", () => window.open("index.html", "_blank"))
-link1.addEventListener("mouseover", () => perso.classList.add('animacao_fase1'))
+link1.addEventListener("click", ()=> {perso.style.animation = "fase1 4s ease forwards"; fases++})
+link1.addEventListener("animationend", ()=> perso.style.top = "300px")
 link1.addEventListener("click", ()=> perso.classList.remove("animacao_fase1"))
-link1.addEventListener("animationend", ()=> perso.style.left = "900px")
 link2.addEventListener("click", ()=> window.open("index.html", "_blank"))
 link3.addEventListener("click", ()=> window.open("index.html", "_blank"))
 link4.addEventListener("click", ()=> window.open("index.html", "_blank"))
-
 // const nome = localStorage.getItem('armazem')
-
 // document.getElementById('is').innerText = nome
 console.log(fases)
-function fase1() {
-    perso.classList.remove("animacao_fase1")
-}
 
