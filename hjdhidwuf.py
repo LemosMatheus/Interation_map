7e7fa8a08af661003021e4a189a9f7b3e8b47b27
yok colecionador de passados <loyvian.l@escolar.ifrn.edu.br>
2025-10-14 16:06:26 -0300

    apagar arquivo

--- hjdhidwuf.py
+++
@@ -1,6 +0,0 @@
-import time
-while True:
-    nome = "mete com forca e com talento estou ofegante e vc percebendo bate e maltrata a sua puta safada quero ejetada de leite na cara mas calma ai nao goza agora quero vc socando minha xota bota ki bota bota ki bota bota ki bota "
-    for i in nome:
-        print(i)
-        time.sleep(0.1)
