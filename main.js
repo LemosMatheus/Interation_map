f38740dd1ee1bcc92359425a08f166dae1234646
0-Rafael <andrerafaelduval@gmail.com>
2025-10-13 22:03:10 -0300

    Alteração redirecionamento do perso

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ const link3 = document.getElementById("link3")
 const link4 = document.getElementById("link4")
 const perso = document.getElementById("personagem")
 let fases = 0
-// link1.addEventListener("click", () => window.open("index.html", "_blank"))
 link1.addEventListener("click", ()=> {perso.style.animation = "fase1 4s ease forwards"; fases++})
-link1.addEventListener("animationend", ()=> perso.style.top = "300px")
+// link1.addEventListener("click", () => {window.open("index.html", "_blank"); })
+perso.addEventListener("animationend",()=> {window.open("index.html", "_blank")})
 link1.addEventListener("click", ()=> perso.classList.remove("animacao_fase1"))
 link2.addEventListener("click", ()=> window.open("index.html", "_blank"))
 link3.addEventListener("click", ()=> window.open("index.html", "_blank"))
@@ -14,4 +14,3 @@ link4.addEventListener("click", ()=> window.open("index.html", "_blank"))
 // const nome = localStorage.getItem('armazem')
 // document.getElementById('is').innerText = nome
 console.log(fases)
-
