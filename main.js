c84194376ce5b86e8444bf11faaa9b22032f5f9a
0-Rafael <andrerafaelduval@gmail.com>
2025-10-18 15:04:06 -0300

    alteração 9

--- main.js
+++ main.js
@@ -4,18 +4,23 @@ const link3 = document.getElementById("link3_mapa1")
 const link4 = document.getElementById("link4_mapa1")
 const perso1 = document.getElementById("personagem_mapa1")
 
+
+const bmap2 = document.getElementById("Bmap2")
 const link1_mapa2 = document.getElementById("link1_mapa2")
 const link2_mapa2 = document.getElementById("link2_mapa2")
 const link3_mapa2 = document.getElementById("link3_mapa2")
 const link4_mapa2 = document.getElementById("link4_mapa2")
 const perso2 = document.getElementById("personagem_mapa2")
 
+const bmap3 = document.getElementById("Bmap3")
 const link1_mapa3 = document.getElementById("link1_mapa3")
 const link2_mapa3 = document.getElementById("link2_mapa3")
 const link3_mapa3 = document.getElementById("link3_mapa3")
 const link4_mapa3 = document.getElementById("link4_mapa3")
 const perso3 = document.getElementById("personagem_mapa3")
 
+
+const bmap4 = document.getElementById("Bmap4")
 const link1_mapa4 = document.getElementById("link1_mapa4")
 const link2_mapa4 = document.getElementById("link2_mapa4")
 const link3_mapa4 = document.getElementById("link3_mapa4")
@@ -24,18 +29,18 @@ const perso4 = document.getElementById("personagem_mapa4")
 
 let fases = 0
 //MAPA 1
-link1.addEventListener("click", ()=> {perso1.style.animation = "fase1_mapa1 4s ease forwards"; fases++; link1.style.filter = "brightness(75%)"; link2.style.backgroundImage = "url(/midia/upscalemedia-transformed.png)"; link2.style.top = "100px"})
+link1.addEventListener("click", ()=> {perso1.style.animation = "fase1_mapa1 4s ease forwards"; fases++; link1.style.filter = "brightness(75%)"; link2.style.backgroundImage = "url(/midia/upscalemedia-transformed.png)"; link2.style.top = "30px";})
 link1.addEventListener("dblclick",()=> {window.open("index.html", "_blank")})
 // link1.addEventListener("click", ()=> perso.classList.remove("animacao_fase1"))
 
-link2.addEventListener("click", ()=> {perso1.style.animation = "fase2_mapa1 4s ease forwards"; link2.style.filter = "brightness(75%)"; link3.style.backgroundImage = "url(/midia/upscalemedia-transformed.png)"; link3.style.top = "145px"})
+link2.addEventListener("click", ()=> {perso1.style.animation = "fase2_mapa1 4s ease forwards"; link2.style.filter = "brightness(75%)"; link3.style.backgroundImage = "url(/midia/upscalemedia-transformed.png)"; link3.style.top = "50px"})
 link2.addEventListener("dblclick", ()=> window.open("index.html", "_blank"))
 
-link3.addEventListener("click", () => {perso1.style.animation = "fase3_mapa1 4s ease forwards"; link3.style.filter = "brightness(75%)"; link4.style.backgroundImage = "url(/midia/upscalemedia-transformed.png)"; link4.style.top = "180px"})
+link3.addEventListener("click", () => {perso1.style.animation = "fase3_mapa1 4s ease forwards"; link3.style.filter = "brightness(75%)"; link4.style.backgroundImage = "url(/midia/upscalemedia-transformed.png)"; link4.style.top = "70px"})
 link3.addEventListener("dblclick", ()=> window.open("index.html", "_blank"))
 
 link4.addEventListener("click", ()=> {perso1.style.animation = "fase4_mapa1 4s ease forwards"; link4.style.filter = "brightness(75%)"})
-link4.addEventListener("dblclick", ()=> window.open("index.html", "_blank"))
+link4.addEventListener("dblclick", ()=> {window.open("index.html", "_blank"); bmap2.style.display = "none"})
 // const nome = localStorage.getItem('armazem')
 // document.getElementById('is').innerText = nome
 
@@ -50,10 +55,10 @@ link3_mapa2.addEventListener("click", ()=> {perso2.style.animation = "fase3_mapa
 link3_mapa2.addEventListener("dblclick", ()=> window.open("inde.html", "_blank"))
 
 link4_mapa2.addEventListener("click", ()=> {perso2.style.animation = "fase4_mapa2 4s ease forwards"; link4_mapa2.style.filter = "brightness(75%)"})
-link4_mapa2.addEventListener("dblclick", ()=> window.open("index.html", "_blank"))
+link4_mapa2.addEventListener("dblclick", ()=> {window.open("index.html", "_blank"); bmap3.style.display = "none"})
 
 //MAPA3
-link1_mapa3.addEventListener("click", ()=> {perso3.style.animation = "fase1_mapa3 4s ease forwards"; link1_mapa3.style.filter = "brightness(75%)"; link2_mapa3.style.backgroundImage = "url(/midia/upscalemedia-transformed.png)"; link2_mapa3.style.top = "33vh"})
+link1_mapa3.addEventListener("click", ()=> {perso3.style.animation = "fase1_mapa3 4s ease forwards"; link1_mapa3.style.filter = "brightness(75%)"; link2_mapa3.style.backgroundImage = "url(/midia/upscalemedia-transformed.png)"})
 link1_mapa3.addEventListener("dblclick", ()=> window.open("index.html", "_blank"))
 
 link2_mapa3.addEventListener("click", ()=> {perso3.style.animation = "fase2_mapa3 4s ease forwards"; link2_mapa3.style.filter = "brightness(75%)"; link3_mapa3.style.backgroundImage = "url(/midia/upscalemedia-transformed.png)"})
@@ -63,7 +68,7 @@ link3_mapa3.addEventListener("click", ()=> {perso3.style.animation = "fase3_mapa
 link3_mapa3.addEventListener("dblclick", ()=> window.open("index.html", "_blank"))
 
 link4_mapa3.addEventListener("click", ()=> {perso3.style.animation = "fase4_mapa3 4s ease forwards"; link4_mapa3.style.filter = "brightness(75%)"})
-link4_mapa3.addEventListener("dblclick", ()=> window.open("index.html", "_blank"))
+link4_mapa3.addEventListener("dblclick", ()=> {window.open("index.html", "_blank"); bmap4.style.display = "none"})
 
 //MAPA4
 link1_mapa4.addEventListener("click", ()=> {perso4.style.animation = "fase1_mapa4 4s ease forwards"; link1_mapa4.style.filter = "brightness(75%)"; link2_mapa4.style.backgroundImage = "url(/midia/upscalemedia-transformed.png)"})
@@ -73,5 +78,5 @@ link2_mapa4.addEventListener("click", ()=> {perso4.style.animation = "fase2_mapa
 link2_mapa4.addEventListener("dblclick", ()=> window.open("index.html", "_blank"))
 
 link3_mapa4.addEventListener("click", ()=> {perso4.style.animation = "fase3_mapa4 4s ease forwards"; link3_mapa4.style.filter = "brightness(75%)"})
-link3_mapa4.addEventListener("dblclick", ()=> window.open("inxe.html", "_blank"))
+link3_mapa4.addEventListener("dblclick", ()=> window.open("index.html", "_blank"))
 console.log(fases)
